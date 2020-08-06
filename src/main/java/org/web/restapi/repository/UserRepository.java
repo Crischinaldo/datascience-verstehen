140da355b71c9f79be569d64b46eb0871d8d443f
Crischinaldo <temp566566@gmx.de>
2020-08-06 17:35:50 +0200

    now implements jpaspecexecutor API

--- src/main/java/org/web/restapi/repository/UserRepository.java
+++ src/main/java/org/web/restapi/repository/UserRepository.java
@@ -1,13 +1,18 @@
 package org.web.restapi.repository;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.web.restapi.model.User;
 
 import java.util.List;
 
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
+
+
 
 }
