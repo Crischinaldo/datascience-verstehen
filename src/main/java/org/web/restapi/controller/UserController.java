cea1bc9392adc3730cc71407d5de371d206175f5
Crischinaldo <temp566566@gmx.de>
2020-08-07 13:04:56 +0200

    modified UserNotFound exc handling

--- src/main/java/org/web/restapi/controller/UserController.java
+++ src/main/java/org/web/restapi/controller/UserController.java
@@ -1,16 +1,17 @@
 package org.web.restapi.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.web.restapi.exception.ApiError;
 import org.web.restapi.exception.UserNotFoundException;
 import org.web.restapi.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.web.restapi.repository.UserRepository;
 import org.web.restapi.service.UserService;
-import org.web.restapi.specification.UserSpecifications;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -24,8 +25,13 @@ public class UserController {
             produces = MediaType.APPLICATION_JSON_VALUE,
             value = "/users"
     )
-    public List<User> getUsers(){
-        return userService.getUsers();
+    public ResponseEntity<List<User>> getUsers(){
+        List<User> usersResult = userService.getUsers();
+
+        if (!usersResult.isEmpty()) {
+            return new ResponseEntity<>(usersResult, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @RequestMapping(
@@ -33,6 +39,7 @@ public class UserController {
             produces = MediaType.APPLICATION_JSON_VALUE,
             value = "/users/{id}"
     )
+    @ResponseStatus(HttpStatus.OK)
     public User getUserById(@PathVariable Long id) throws UserNotFoundException {
         return userService.getUserById(id);
     }
@@ -43,6 +50,7 @@ public class UserController {
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
+    @ResponseStatus(HttpStatus.CREATED)
     public User addUser(@RequestBody User user) {
         return userService.add(user);
     }
--- src/main/java/org/web/restapi/service/UserService.java
+++ src/main/java/org/web/restapi/service/UserService.java
@@ -1,12 +1,9 @@
 package org.web.restapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.web.restapi.exception.UserNotFoundException;
 import org.web.restapi.model.User;
 import org.web.restapi.repository.UserRepository;
@@ -28,7 +25,7 @@ public class UserService {
 
     public User getUserById(Long id) throws UserNotFoundException {
         return userRepository.findOne(UserSpecifications.getUserByID(id))
-                .orElseThrow(() -> new UserNotFoundException("User with id " + id + " not Found"));
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 
     public User add(@RequestBody User user) {
